a00333161eccd1568c98ad8210c3eaa556a5ead9
Torgy <jctorge@gmail.com>
2022-04-30 12:11:16 -0500

    HUZZAH

--- Develop/README.md
+++ Develop/README.md
@@ -1,33 +1,39 @@
-# yep
+# Yep
 
-  ## Licensing:
-  [![license](https://img.shield.io/badge/license-Apache-blue)](https://shield.io)
+  
+  ![badge](https://img.shields.io/badge/license-GNUAGPLv3-brightgreen)
 
 
   ## Table of Contents
   - [Description](#description)
   - [Installation](#installation)
   - [Usage](#usage)
+  - [License](#license)
   - [Contribution](#contribution)
   - [Testing](#testing)
   - [Questions](#questions)
 
   ## Description:
-  yep
+  Yep
 
   ## Installation:
   yep
 
   ## Usage:
-  yep
+  Yep
+
+  ## License:
+  _This application has the GNUAGPLv3._
+      
+  For more information please view the [license description](https://choosealicense.com/licenses/agpl-3.0/).
 
   ## Contribution:
-  yep
+  Yep
 
   ## Testing:
-  no
+  Nope
 
   ## If any questions please contact at:
-  - GitHub: [yep](https://github.com/yep)
-  - Email: yep
+  - GitHub: [Yep](https://github.com/Yep)
+  - Email: Yep
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,37 +1,68 @@
-// 
-function renderLicenseBadge(license) {
+// // 
+// function renderLicenseBadge(license) {
+//   let licenseChoice = `${data.licensing}`;
+//   let licenseLink = '';
+
+//   if (licenseChoice === 'GNUAGPLv3') {
+//     licenseLink = 'https://choosealicense.com/licenses/agpl-3.0/';
+//   };
+//   if (licenseChoice === 'GNUGPLv3') {
+//     licenseLink = 'https://choosealicense.com/licenses/gpl-3.0/';
+//   };
+//   if (licenseChoice === 'MozillaPublicLicense2.0') {
+//     licenseLink = 'https://choosealicense.com/licenses/mpl-2.0/';
+//   };
+//   if (licenseChoice === 'ApacheLicense2.0') {
+//     licenseLink = 'https://choosealicense.com/licenses/apache-2.0/';
+//   };
+//   if (licenseChoice === 'MITLicense') {
+//     licenseLink = 'https://choosealicense.com/licenses/mit/';
+//   };
+//   if (licenseChoice === 'BoostSoftwareLicense1.0') {
+//     licenseLink = 'https://choosealicense.com/licenses/bsl-1.0/';
+//   };
+//   if (licenseChoice === 'TheUnlicense') {
+//     licenseLink = 'https://choosealicense.com/licenses/unlicense/';
+//   };
+//   if (license === 'None') {
+//     licenseLink === 'N/A';
+//   }
+// }
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
   let licenseChoice = `${data.licensing}`;
   let licenseLink = '';
 
   if (licenseChoice === 'GNUAGPLv3') {
     licenseLink = 'https://choosealicense.com/licenses/agpl-3.0/';
   };
-  if (licenseChoice === '') {
-    licenseLink = '';
+  if (licenseChoice === 'GNUGPLv3') {
+    licenseLink = 'https://choosealicense.com/licenses/gpl-3.0/';
   };
-  if (licenseChoice === '') {
-    licenseLink = '';
+  if (licenseChoice === 'MozillaPublicLicense2.0') {
+    licenseLink = 'https://choosealicense.com/licenses/mpl-2.0/';
   };
-  if (licenseChoice === '') {
-    licenseLink = '';
+  if (licenseChoice === 'ApacheLicense2.0') {
+    licenseLink = 'https://choosealicense.com/licenses/apache-2.0/';
   };
-  if (licenseChoice === '') {
-    licenseLink = '';
+  if (licenseChoice === 'MITLicense') {
+    licenseLink = 'https://choosealicense.com/licenses/mit/';
   };
-  if (licenseChoice === '') {
-    licenseLink = '';
+  if (licenseChoice === 'BoostSoftwareLicense1.0') {
+    licenseLink = 'https://choosealicense.com/licenses/bsl-1.0/';
   };
-  if (licenseChoice === '') {
-    licenseLink = '';
+  if (licenseChoice === 'TheUnlicense') {
+    licenseLink = 'https://choosealicense.com/licenses/unlicense/';
   };
-}
+  if (licenseChoice === 'None') {
+    licenseLink === 'N/A';
+  }
+  
+  let markdownTemplate = `# ${data.title}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-  ## Licensing:
-  [![badge](https://img.shield.io/badge/license-${data.licensing}-blue)](https://shield.io)
+  
+  ![badge](https://img.shields.io/badge/license-${data.licensing}-brightgreen)
 
 
   ## Table of Contents
@@ -68,6 +99,9 @@ function generateMarkdown(data) {
   - Email: ${data.email}
 
 `;
+return markdownTemplate;
+
 }
+
 
 module.exports = generateMarkdown;
